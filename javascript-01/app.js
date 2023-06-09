a4daeedf8efe91df936a564f06bd92ec025c13e5
Hafiz Ahmed Ali Ansari <117213903+HafizZada@users.noreply.github.com>
2023-06-09 14:00:11 +0500

    9-6-2023 first Class of Java Script
    1) Alert Box
    2) Varriables ( legal & illegal )
    3) Dat Type   ( String , Number ,Boolean )

---
+++ javascript-01/app.js
@@ -0,0 +1,58 @@
+//   Simple Alert Box  //
+
+// alert ("Hello World");
+// alert ("Hafiz Ahmed ALi Ansari");
+// alert ("Starting JavaScript 15 Day Crash Course");
+
+// ==================== //
+
+// Varriable Save In Java Script //
+
+// let myname1 = "Ahmed Ali";
+// alert (myname1);
+// let myname2 = "Ahmed Ali Ansari";
+// alert (myname2);
+// let myname3 = "Hafiz Ahmed Ali Ansari";
+// alert (myname3);
+
+// ==================== //
+
+// Varriable Rules //
+// 1) Legal Varriable 2) Illegal Varrible
+
+// * Legal Varriable
+
+// let AhmedAliAnsari; // Camel Case
+// let $AhmedAliAnsari; // Special Caractors
+// let _AhmedAliAnsari; // Special Caractors
+// let myname1; // Special Caractors
+// let my2name; // Special Caractors
+
+// ==================== //
+
+// * ILLegal Varriable
+
+// let Ahmed Ali Ansari; // Spaces
+// let 1AhmedAliAnsari;  //  Varriable Not Accept Like
+// let 1AhmedAliAnsari;  //  Varriable Not Accept Like
+// let name  alert;      //  Reserved Key Word Not Accept   
+
+// ==================== //
+
+// Data Type In Java Script //
+
+// String Type //
+// "Ahmed","xyz@world.com","#","12345" 
+
+// ==================== // 
+
+// Number Type //
+//  let myLuckyNumber = 123; 
+// //  Example // 
+//    1,2,3,4,5,23,3423,43543,-213,4.35,324.54;
+
+// ==================== //
+
+// Boolean Type //
+// True & False //
+// let isTodayMonday = true;
