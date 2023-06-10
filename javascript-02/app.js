94beac9aeaa8ae06b23c9fac3aaf6d7c8ddccc37
Hafiz Ahmed Ali Ansari <117213903+HafizZada@users.noreply.github.com>
2023-06-10 13:00:00 +0500

    10-6-2023 Second Class Of Java Script
    1) Find String
    2) String Concentation
    3) String Concentation With String And Number
    4) Math Operators
    5) Prompt Works
    6) Guess Number
    7) Results

---
+++ javascript-02/app.js
@@ -0,0 +1,120 @@
+
+// Find String //
+
+// let data = "Hello";
+// let data2 = data.includes("H");
+// console.log(data2);
+
+// ======================= //
+
+// String Concentation //
+
+// let data1 = "Hello ";
+// let data2 = "Students";
+
+// let data3 = data1 + data2; // string concentention //
+// console.log (data3); 
+
+// ======================= //
+
+// String Concentation With String And Number //
+
+// Example 01:~
+// let data1 = "5"
+// let data2 = 5;
+
+// let data3 = (data1 + data2); // ( data and data2 concentention )
+// console.log (data3);
+// // Answer : 55 //
+
+// Example 02:~
+// let data1 = "5"
+// let data2 = 5;
+
+// let data3 = (+data1 + data2); //  +(change string Value into Number and then add data2)
+// console.log (data3);
+// // Answer : 10 //
+
+// // Example 03:~
+// let data1 = "5"
+// let data2 = 5;
+
+// let data3 = (parseInt(data1) + data2); //  ParseInt (change string value into Number and then add data2)
+// console.log (data3);
+// // Answer : 10 //
+
+// // Example 04:~
+// let data1 = "5"
+// let data2 = 5;
+
+// let data3 = (Number(data1) + data2); //  Number (change string value into Number and then add data2)
+// console.log (data3);
+// // Answer : 10 //
+
+// ======================= //
+
+// Math Operator //
+
+// let data1 = "5";
+// let data2 =  5;
+
+// console.log((data1 % data2));
+// // Answer :~
+// // + 55 | + / parseInt / Number = 10
+// // - 0
+// // * 25 
+// // / 1 
+// // % 0 
+// // ======================= //
+
+// Prompt Works //
+
+// Example 01 :~
+// let userFavNumber = prompt("Enter Your Favourite Number ? "); // User Input Number //
+// console.log(userFavNumber);
+
+// Example 02 :~
+// let userName = prompt("What is Your Name "); // User Input Name //
+// console.log(userName);
+
+// // Example 03 :~
+// let userName = prompt("What is Your Name "); // User Input Name + Concentation //
+// console.log("Wellcome To My Website " + userName);
+
+// // ============================== //
+
+// Guess Number //
+// BODMAS ("Brackets Open","Divide","Multiply","Addition","Substraction")
+
+// Example 01:~
+// let randomNumber = Math.random();
+// console.log(randomNumber * 10);
+
+// Example 02:~
+// let randomNumber = Math.random();
+// randomNumber *= 10;
+// let roundedNumber = Math.round(randomNumber);
+// console.log(roundedNumber);
+
+// // ============================== //
+
+// Again Example //
+
+// Example 01:~
+// let num = 5 + 5 ;
+// console.log(num);
+
+// Example 02:~
+// let num = 5 + 5 * 2;
+// console.log(num);
+// Answer is : 15
+
+// Example 03:~
+// let num = (5 + 5) * 2;
+// console.log(num);
+// // Answer is : 20
+
+// Example 04:~
+// let num = (5 + 5) * 2 + 10 % 2 - 5;
+// console.log(num);
+// // Answer is : 15
